7ee889e9e0e85e80d8cf4e794744c5bc00801573
Anum Rajput <165173136+noorthedev@users.noreply.github.com>
2025-12-17 15:23:08 +0500

    Physical AI & Humanoid Robotics

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://noorthedev.github.io',
+  url: 'https://physical-ai-humanoid-robotics-hackathon-1-acfffmzkr.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
